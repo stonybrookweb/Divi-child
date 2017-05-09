ebfeeb4cd5b734f43ff4b804a4717136e272b274
Alana Martin <alana@stonybrookweb.com>
2017-05-08 21:54:05 -0400

    refactor: move sort event code inside jQuery Document ready

--- scripts/site.js
+++ scripts/site.js
@@ -7,16 +7,10 @@ var mempressUnauthorized = jQuery("div").hasClass("mepr-unauthorized-message");
 // Are we on a page were the Divi page builder is used?
 var pageBuilderUsed = jQuery("body").hasClass("et_pb_pagebuilder_layout");
 
-
-console.log("unauthorized: " + mempressUnauthorized + " page builder used: " + pageBuilderUsed);
 // If so add mempressUnauthorized class to body for additional styling
 if (mempressUnauthorized == true && pageBuilderUsed == true){
       jQuery("body").addClass("mempressUnauthorized");
 }
-
-});
-
-
 
 // Quick temporary fix for sorting eventbrite events
 if(jQuery("body").hasClass("page-template-past-eventbrite-index")){
@@ -24,3 +18,5 @@ if(jQuery("body").hasClass("page-template-past-eventbrite-index")){
   articles = jQuery("#content-area article").reverse().detach();
   jQuery("#content-area").append(articles);
 }
+
+});
